a017ddf762fc027178face81a503959e7d423a56
fariha habib <farihahabib2202@gmail.com>
2025-12-14 19:36:41 +0600

    add all

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
@@ -40,6 +41,7 @@ const verifyJWT = async (req, res, next) => {
   }
 }
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
@@ -55,20 +57,102 @@ async function run() {
   const applyCollection = db.collection('applys')
   const usersCollection = db.collection('users')
   const reviewsCollection = db.collection('reviews')
+ //veryfy admin
+    const verifyADMIN = async (req, res, next) => {
+      const email = req.tokenEmail
+      const user = await usersCollection.findOne({ email })
+      if (user?.role !== 'admin')
+        return res
+          .status(403)
+          .send({ message: 'Admin only Actions!', role: user?.role })
+
+      next()
+    }
+//verify moderator
+    const verifyMODERATOR = async (req, res, next) => {
+      const email = req.tokenEmail
+      const user = await usersCollection.findOne({ email })
+      if (user?.role !== 'moderator')
+        return res
+          .status(403)
+          .send({ message: 'moderator only Actions!', role: user?.role })
+
+      next()
+    }
+    
  //post scholarship data
-  app.post('/scholarships',async(req,res)=>{
+  app.post('/scholarships',verifyJWT,verifyADMIN,async(req,res)=>{
     const scholarshipData = req.body;
     const result = await scholarshipCollection.insertOne(scholarshipData)
     res.send(result)
   })
 //get all scholarships
-app.get('/scholarships',async(req,res)=>{
-const result = await scholarshipCollection.find().toArray()
-  res.send(result)
+app.get('/scholarships',verifyJWT,verifyADMIN, async (req, res) => {
+  try {
+    const {
+      search = '',
+      country = '',
+      sortBy = 'fees_asc',
+      page = 1,
+      limit = 8
+    } = req.query;
 
-})
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+    const skip = (pageNumber - 1) * limitNumber;
 
-app.delete('/scholarships/:id',async(req,res)=>{
+    /* -------- FILTER -------- */
+    const query = {};
+
+    //  Search by Scholarship Name 
+    if (search) {
+      query.scholarshipName = {
+        $regex: search,
+        $options: 'i',
+      };
+    }
+
+    //  Filter by Country
+    if (country) {
+      query.country = country;
+    }
+
+    /* -------- SORT (Application Fees) -------- */
+    let sortQuery = {};
+
+    if (sortBy === 'fees_asc') {
+      sortQuery.applicationFees = 1;
+    }
+
+    if (sortBy === 'fees_desc') {
+      sortQuery.applicationFees = -1;
+    }
+
+    /* -------- DB QUERY -------- */
+    const scholarships = await scholarshipCollection
+      .find(query)
+      .sort(sortQuery)
+      .skip(skip)
+      .limit(limitNumber)
+      .toArray();
+
+    const total = await scholarshipCollection.countDocuments(query);
+    const pages = Math.ceil(total / limitNumber);
+
+    res.send({
+      scholarships,
+      total,
+      pages,
+      currentPage: pageNumber,
+    });
+
+  } catch (error) {
+    res.status(500).send({ message: 'Server Error' });
+  }
+});
+
+//delete one scholarship
+app.delete('/scholarships/:id',verifyJWT,verifyADMIN,async(req,res)=>{
   const {id} = req.params
 const objectId = new ObjectId(id)
 const filter = {_id: objectId}
@@ -84,7 +168,7 @@ app.get('/scholarships/:id',async(req,res)=>{
      res.send(result)
 })
 //delete single scholarship
-app.delete('/scholarships/:id', async (req, res) => {
+app.delete('/scholarships/:id',verifyJWT,verifyADMIN, async (req, res) => {
 
     const id = req.params.id;
     const result = await scholarshipCollection.deleteOne({ _id: new ObjectId(id) });
@@ -96,7 +180,7 @@ app.delete('/scholarships/:id', async (req, res) => {
     res.json({ message: 'Scholarship deleted successfully' });
 });
 //update/scholarship/id
-app.put('/scholarships/:id', async (req, res) => {
+app.put('/scholarships/:id',verifyJWT,verifyADMIN, async (req, res) => {
   try {
     const id = req.params.id;
     const newData = req.body; // must contain all fields of the scholarship
@@ -125,91 +209,23 @@ app.put('/scholarships/:id', async (req, res) => {
 
 
 
-// payment endpoint
- app.post('/create-checkout-session', async (req, res) => {
-      const paymentInfo = req.body;
-      console.log(paymentInfo)
-      const session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price_data: {
-              currency: 'usd',
-         unit_amount: paymentInfo?.applicationFees * 100,
-              product_data: {
-                name: paymentInfo?.scholarshipName,
-                images: [paymentInfo.image],
-              },
-             
-            },
-            quantity: 1,
-          },
-        ],
-        customer_email: paymentInfo?.Student?.email,
 
-        
-        mode: 'payment',
-        metadata: {
-          scholarshipId: paymentInfo?.scholarshipId,
-          customer: paymentInfo?.Student?.email,
-        },
-        success_url: `${process.env.CLIENT_DOMAIN}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${process.env.CLIENT_DOMAIN}/dashboard`,
-      })
-      res.send({ url: session.url })
-    })
-
-app.post('/payment-success', async (req, res) => {
-      const { sessionId } = req.body
-      const session = await stripe.checkout.sessions.retrieve(sessionId)
-      
-      console.log('payment-session',session)
-      const  scholarship = await scholarshipCollection.findOne({_id:
-         new ObjectId(session.metadata.scholarshipId)})
-         console.log('scholarship',scholarship)
-      console.log(session)
-      const apply = await applyCollection.findOne({
-        tranjectionId: session.payment_intent,
-      })
-      if (session.status === 'complete' && scholarship && !apply) {
-        const applyInfo = {
-          scholarshipId: session.metadata.scholarshipId,
-          tranjectionId: session.payment_intent,
-          student : session.metadata.customer,
-          studentName : scholarship.moderator.name,
-          studentEmail : session.customer_details.email,
-          moderator:session.moderator,
-          status: 'pending',
-          scholarshipName: scholarship.scholarshipName,
-          universityName: scholarship.universityName,
-          applicationFees: scholarship.applicationFees,
-          country: scholarship.country,
-          city: scholarship.city,
-          degree: scholarship.degree,
-          subjectCategory: scholarship.subjectCategory,
-
-          amount: session.amount_total / 100,
-        }
-          const result = await applyCollection.insertOne(applyInfo)
-      }
-      res.send(scholarship)
-
-})
 
 //get all applys for a student by email
 app.get('/my-applications/:email',async(req,res)=>{
 const email = req.params.email;
-const result = await applyCollection.find({student:email}).toArray()
+const result = await applyCollection.find({studentEmail:email}).toArray()
   res.send(result)
 })
 //get all applys 
-app.get('/applications',async(req,res)=>{
+app.get('/applications',verifyJWT,verifyMODERATOR,async(req,res)=>{
 const result = await applyCollection.find().toArray()
   res.send(result)
 
 })
 
 // update application status
-app.patch('/applications/status/:id',async(req,res)=>{
+app.patch('/applications/status/:id',verifyJWT,verifyMODERATOR,async(req,res)=>{
   const id = req.params.id;
   const {status} = req.body;
   const objectId = new ObjectId(id);
@@ -223,7 +239,7 @@ app.patch('/applications/status/:id',async(req,res)=>{
 
 })
 //post review
-app.post("/reviews", async (req, res) => {
+app.post("/reviews",verifyJWT, async (req, res) => {
   const review = req.body;
 
   const alreadyReviewed = await reviewsCollection.findOne({
@@ -239,18 +255,18 @@ app.post("/reviews", async (req, res) => {
   res.send(result);
 });
 //get review
-app.get("/reviews", async (req, res) => {
+app.get("/reviews",verifyJWT,verifyMODERATOR, async (req, res) => {
   const reviews = await reviewsCollection.find({}).toArray();
   res.send(reviews);
 });
 //get single reviews by email
-app.get('/my-reviews/:email',async(req,res)=>{
+app.get('/my-reviews/:email',verifyJWT,async(req,res)=>{
 const email = req.params.email;
 const result = await reviewsCollection.find({ studentEmail: email }).toArray();
   res.send(result)
 })
 //delete review
-app.delete("/reviews/:id", async (req, res) => {
+app.delete("/reviews/:id",verifyJWT,verifyMODERATOR, async (req, res) => {
   const { id } = req.params;
     const result = await reviewsCollection.deleteOne({ _id: new ObjectId(id) });
 
@@ -262,7 +278,7 @@ app.delete("/reviews/:id", async (req, res) => {
   
 });
 // Delete a review by ID, only if it belongs to the user
-app.delete("/my-reviews/:id/:email", async (req, res) => {
+app.delete("/my-reviews/:id/:email",verifyJWT, async (req, res) => {
   const { id, email } = req.params;
 const result = await reviewsCollection.deleteOne({
   _id: new ObjectId(id),
@@ -278,7 +294,7 @@ const result = await reviewsCollection.deleteOne({
   
 });
 //edit review
-app.patch('/reviews/:id', async (req, res) => {
+app.patch('/reviews/:id',verifyJWT, async (req, res) => {
   const id = req.params.id;
   const { rating, comment } = req.body;
     const result = await reviewsCollection.updateOne(
@@ -292,13 +308,6 @@ app.patch('/reviews/:id', async (req, res) => {
 
   } 
 });
-//get all applys for moderator by email
-app.get('/moderator-applications/:email',async(req,res)=>{
-const email = req.params.email;
-const result = await scholarshipCollection.find({'moderator.email':email}).toArray()
-  res.send(result
-  )  }
-)
 //save users in database
  app.post('/users', async (req, res) => {
       const userData = req.body
@@ -329,13 +338,13 @@ const result = await scholarshipCollection.find({'moderator.email':email}).toArr
     })
 
 //get all users
-app.get('/users', async (req, res) => {
+app.get('/users',verifyJWT,verifyADMIN, async (req, res) => {
   const users = await usersCollection.find().toArray();
   res.send(users);
 });
 
 // Update role
-app.patch('/users/:id/role', async (req, res) => {
+app.patch('/users/:id/role',verifyJWT,verifyADMIN, async (req, res) => {
   const { role } = req.body;
   const { id } = req.params;
   const result = await usersCollection.updateOne(
@@ -346,31 +355,102 @@ app.patch('/users/:id/role', async (req, res) => {
 });
 
 // Delete user
-app.delete('/users/:id', async (req, res) => {
+app.delete('/users/:id',verifyJWT,verifyADMIN, async (req, res) => {
   const { id } = req.params;
   const result = await usersCollection.deleteOne({ _id: new ObjectId(id) });
   res.send(result);
 });
+
+// payment endpoint
+ app.post('/create-checkout-session',verifyJWT, async (req, res) => {
+      const paymentInfo = req.body;
+      console.log('paymentinfo',paymentInfo)
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: 'usd',
+         unit_amount: paymentInfo?.applicationFees * 100,
+              product_data: {
+                name: paymentInfo?.scholarshipName,
+                images: [paymentInfo.image],
+              },
+             
+            },
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo?.Student?.email,
+
+        
+        mode: 'payment',
+        metadata: {
+          scholarshipId: paymentInfo?.scholarshipId,
+          customer: paymentInfo?.Student?.name,
+        },
+        success_url: `${process.env.CLIENT_DOMAIN}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.CLIENT_DOMAIN}/dashboard`,
+      })
+      res.send({ url: session.url })
+    })
+//get payment data
+app.post('/payment-success',verifyJWT, async (req, res) => {
+      const { sessionId } = req.body
+      const session = await stripe.checkout.sessions.retrieve(sessionId)
+      
+      console.log('payment-session',session)
+      const  scholarship = await scholarshipCollection.findOne({_id:
+         new ObjectId(session.metadata.scholarshipId)})
+         console.log('scholarship',scholarship)
+      console.log(session)
+      const apply = await applyCollection.findOne({
+        tranjectionId: session.payment_intent,
+      })
+      if (session.status === 'complete' && scholarship && !apply) {
+        const applyInfo = {
+          scholarshipId: session.metadata.scholarshipId,
+          tranjectionId: session.payment_intent,
+          student : session.metadata.customer,
+          studentName : session.metadata.customer,
+          studentEmail : session.customer_email,
+          moderator:session.moderator,
+          status: 'pending',
+          scholarshipName: scholarship.scholarshipName,
+          universityName: scholarship.universityName,
+          applicationFees: scholarship.applicationFees,
+          country: scholarship.country,
+          city: scholarship.city,
+          degree: scholarship.degree,
+          subjectCategory: scholarship.subjectCategory,
+          paymentStatus: session. payment_status,
+          amount: session.amount_total / 100,
+        }
+          const result = await applyCollection.insertOne(applyInfo)
+      }
+      res.send(scholarship)
+
+})
+
 //for analytics
 //total users
-app.get('/analytics/total-users', async (req, res) => {
+app.get('/analytics/total-users',verifyJWT,verifyADMIN, async (req, res) => {
   const totalUsers = await usersCollection.countDocuments();
   res.send({ totalUsers });
 });
 //total scholarships
-app.get('/analytics/total-scholarships', async (req, res) => {
+app.get('/analytics/total-scholarships',verifyJWT,verifyADMIN, async (req, res) => {
   const totalScholarships = await scholarshipCollection.countDocuments();
   res.send({ totalScholarships });
 });
 //total applicatons
-app.get('/analytics/total-fees', async (req, res) => {
+app.get('/analytics/total-fees',verifyJWT,verifyADMIN,  async (req, res) => {
   const result = await applyCollection.aggregate([
     { $group: { _id: null, totalFees: { $sum: "$applicationFees" } } }
   ]).toArray();
   res.send({ totalFees: result[0]?.totalFees || 0 });
 });
 //applications by university
-app.get('/analytics/applications-chart', async (req, res) => {
+app.get('/analytics/applications-chart',verifyJWT,verifyADMIN,  async (req, res) => {
   const data = await applyCollection.aggregate([
     { $group: { _id: "$universityName", applications: { $sum: 1 } } }
   ]).toArray();
